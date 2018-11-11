50248964ca13669d595aeeb6981f6aad33f4b6f9
mrfleap <mrfleap@gmail.com>
2018-11-11 00:10:00 -0800

    Major progress on backend and various aspects of the website

--- app/forgyftapp/views/basic_views.py
+++ app/forgyftapp/views/basic_views.py
@@ -1,18 +1,36 @@
 from django.contrib.admin.views.decorators import staff_member_required
+from django.contrib.auth.decorators import login_required
 from django.db import transaction
 from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 
 # Create your views here.
+from django.template.response import TemplateResponse
 from django.urls import reverse_lazy, reverse
 from django.views.generic import CreateView, ListView
 
 from forgyftapp.forms import GifteeProfileForm, GiftIdeaForm, GiftIdeaFormSet
 from forgyftapp.messaging import broadcast_to_slack
-from forgyftapp.models import GifteeProfile
+from forgyftapp.models import GifteeProfile, GiftIdea
 
 
 def index(request):
+	return render(request, "homepage.html", {"page": "home"})
+
+@login_required
+def gift_request(request, profile=None):
+	if profile:
+		giftee_profile = get_object_or_404(GifteeProfile, pk=profile)
+
+		return render(request, "request.html", {"giftee_profile": giftee_profile,
+		                                        "ideas": GiftIdea.objects.filter(giftee_profile=giftee_profile),
+		                                        "page": "request"})
+	else:
+		giftee_profiles = GifteeProfile.objects.filter(user=request.user)
+
+		return render(request, "request.html", {"giftee_profiles": giftee_profiles, "page": "request"})
+
+def gift_form(request):
 	if request.method == "POST":
 		form = GifteeProfileForm(request.POST)
 		if form.is_valid():
@@ -21,54 +39,62 @@ def index(request):
 	else:
 		form = GifteeProfileForm()
 
-	return render(request, "homepage.html", {"form": form})
+	return render(request, "gift_form.html", {"form": form, "page": "request"})
 
-
-class GifteeList(ListView):
-	model = GifteeProfile
-
-
-class GiftIdeaCreate(CreateView):
-	model = GifteeProfile
-	fields = ['interests']
-	success_url = reverse_lazy('forgyftapp:giftee-list')
-
-	def get_context_data(self, **kwargs):
-		data = super(GiftIdeaCreate, self).get_context_data(**kwargs)
-		if self.request.POST:
-			data['giftideas'] = GiftIdeaFormSet(self.request.POST)
-		else:
-			data['giftideas'] = GiftIdeaFormSet()
-		return data
-
-	def form_valid(self, form):
-		context = self.get_context_data()
-		giftideas = context['giftideas']
-		with transaction.atomic():
-			self.object = form.save()
-
-			if giftideas.is_valid():
-				giftideas.instance = self.object
-				giftideas.save()
-		return super(GiftIdeaCreate, self).form_valid(form)
 
 @staff_member_required
 def fulfill(request, profile=None):
-
 	if profile:
 		giftee_profile = get_object_or_404(GifteeProfile, pk=profile)
 
+		# if request.method == "POST":
+		# 	form = GiftIdeaForm(request.POST)
+		# 	if form.is_valid():
+		# 		form.save()
+		# 		return redirect("forgyftapp:index")
+		# else:
+		# 	form = GiftIdeaForm()
+
 		if request.method == "POST":
-			form = GiftIdeaForm(request.POST)
-			if form.is_valid():
-				form.save()
-				return redirect("forgyftapp:index")
+			# data = dict(request.POST)
+			# for element in data:
+			# 	data[element] = data[element][0]
+			#
+			# for key in iter(data):
+			# 	if key.endswith("published"):
+			# 		if data[key] == "on":
+			# 			data[key] = "true"
+			# 		else:
+			# 			data[key] = "false"
+
+			gift_ideas = GiftIdeaFormSet(request.POST, instance=giftee_profile)
+			i = 0
+			for form in gift_ideas:
+				if len(form.changed_data) == 1 and form.changed_data[0] == "published":
+					form.changed_data = []
+				i += 1
+			if gift_ideas.is_valid():
+				gift_ideas.save()
+
+				published = False
+				for form in gift_ideas:
+					if len(gift_ideas.forms[0].cleaned_data) > 0 and gift_ideas.forms[0].cleaned_data["published"]:
+						published = True
+
+				if published:
+					giftee_profile.submit()
+				else:
+					giftee_profile.unsubmit()
 		else:
-			form = GiftIdeaForm()
+			gift_ideas = GiftIdeaFormSet(instance=giftee_profile)
 
+		published = giftee_profile.published or (hasattr(gift_ideas.forms[0], "cleaned_data") and gift_ideas.forms[0].cleaned_data["published"])
 
-		return render(request, "fulfill.html", {"giftee_profile": giftee_profile, "form": form})
+		return render(request, "fulfill.html", {"giftee_profile": giftee_profile, "gift_ideas": gift_ideas,
+		                                        "page": "fulfill", "published": published})
 	else:
-		giftee_profiles = GifteeProfile.objects.filter(fulfilled=False)
+		giftee_profiles_unpublished = GifteeProfile.objects.filter(published=False)
+		giftee_profile_published = GifteeProfile.objects.filter(published=True)
 
-		return render(request, "fulfill.html", {"giftee_profiles": giftee_profiles})
+		return render(request, "fulfill.html", {"giftee_profiles_unpublished": giftee_profiles_unpublished,
+		                                        "page": "fulfill", "giftee_profile_published": giftee_profile_published})
