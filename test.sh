64a698c7c0443436a18a0d53fe30d91bdfa249ed
mrfleap <mrfleap@gmail.com>
2018-11-27 19:53:32 -0800

    Added test that tests to see if the web server is running with a 200 response

--- test.sh
+++ test.sh
@@ -1,7 +1,40 @@
 #!/usr/bin/env bash
 
 COLLECT_STATIC=0
-docker-compose -f docker-compose.yml -f docker-compose.prod.yml build --compress
-docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d
-docker-compose -f docker-compose.yml -f docker-compose.prod.yml ps
+docker-compose down
+docker-compose build --compress
+docker-compose up -d
+docker-compose ps
+
+attempt_counter=0
+max_attempts=15
+
+until $(curl --output /dev/null --silent --head --fail http://localhost); do
+    if [ ${attempt_counter} -eq ${max_attempts} ];then
+      echo "Max attempts reached"
+      exit 1
+    fi
+
+    printf '.'
+    attempt_counter=$(($attempt_counter+1))
+    sleep 1
+done
+
+
+web_response=`curl -sL \
+    -w "%{http_code}\\n" \
+    "http://localhost" \
+    -o /dev/null \
+    --connect-timeout 10 \
+    --max-time 30`
+
+echo $web_response
+
+if [[ $web_response == "200" ]] ;
+then
+   echo "Server OK" ;
+else
+   echo "Server not OK, responded with $web_response" ;
+   exit 1
+fi
 exit 0
