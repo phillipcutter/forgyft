3cf8fd38f5fc4fc95b0415a70777d4cec307f686
Phillip Cutter <mrfleap@gmail.com>
2018-12-31 13:08:48 -0800

    Scraper optimizations

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -8,24 +8,25 @@ def load():
 	with open("output.txt", "rb") as dill_file:
 		interests = dill.load(dill_file)
 	print(interests)
+	return interests
 
 class Interest:
-	interest = ""
-	plural = ""
-	links = []
-	products = []
-
 	def __init__(self, interest, plural):
 		self.interest = interest
 		self.plural = plural
+		self.links = []
+		self.products = []
 
 
 def get_links(url):
 	r = requests.get(api_url, params={"token": api_token, "url": url, "fields": "links"})
-	if not r.json().get("errorCode", None):
-		links = r.json().get("objects", [None])[0].get("links")
-		return filter_links(links)
-	else:
+	try:
+		if not r.json().get("errorCode", None):
+			links = r.json().get("objects", [None])[0].get("links")
+			return filter_links(links)
+		else:
+			return []
+	except:
 		return []
 
 
@@ -44,19 +45,23 @@ interests = [
 	Interest("music", "music lovers")
 ]
 
-for interest in interests:
-	pages = 2
-	results = google.search("Gifts for " + interest.plural, pages)
-	for result in results:
-		interest.links.append(result.link)
+if __name__ == "__main__":
+	for interest in interests:
+		pages = 2
+		results = google.search("Gifts for " + interest.plural, pages)
+		# print(interest.plural + " ideas")
+		for result in results:
+			interest.links.append(result.link)
+		# print(str(len(results)) + " results")
+		# print(str(len(interest.links)) + " links")
 
-for interest in tqdm(interests):
-	for url in tqdm(interest.links):
-		links = get_links(url)
-		interest.products.extend(links)
+	for interest in tqdm(interests):
+		for url in tqdm(interest.links):
+			links = get_links(url)
+			interest.products.extend(links)
 
-print("Done")
-print("Pickle: " + str(dill.dumps(interests)))
+	print("Done")
+	print("Pickle: " + str(dill.dumps(interests)))
 
-with open("output.txt", "wb") as dill_file:
-	dill.dump(interests, dill_file)
+	with open("output.txt", "wb") as dill_file:
+		dill.dump(interests, dill_file)
